830670fd53f81b5c6d8215a65c031aa77db3ad58
xiaobuding <2223143694@qq.com>
2023-09-19 16:52:47 +0800

    成功将SDL2引入到Clion中，要点:将SDL的文件放到MingW下，具体看浏览器Clion收藏夹

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include "SDL2/SDL.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[])
+{
+    // 初始化SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    // 创建SDL窗口指针
+    SDL_Window *window = SDL_CreateWindow(
+            "SDL2Test",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            0
+    );
+
+    // 创建渲染器
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    // 设置渲染器
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    // 渲染
+    SDL_RenderPresent(renderer);
+
+    // 等待3000ms
+    SDL_Delay(3000);
+
+    // 销毁窗口&退出
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
